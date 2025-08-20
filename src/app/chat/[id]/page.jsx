ebf0af5c455eab42e00cf0557018f9a4c6e3527e
Akash <ak.parmar1442000@gmail.com>
2025-08-20 23:03:41 +0530

    Update chat layout and styling. Change height calculations to use dynamic viewport height (dvh) for better responsiveness. Add multiple message components for improved chat display and modify input container to be fixed at the bottom of the chat section.

--- src/app/chat/[id]/page.jsx
+++ src/app/chat/[id]/page.jsx
@@ -4,7 +4,7 @@ import { UserCircle } from 'lucide-react'
 
 const page = () => {
   return (
-    <section className='h-[calc(100vh-69px)] overflow-y-scroll overflow-x-hidden pt-2 sm:pt-4 px-2 sm:px-4 w-full md:max-w-3xl mx-auto relative scrollbar-hide'>
+    <section className='h-[calc(100dvh-69px-16px)] overflow-y-scroll overflow-x-hidden pt-2 sm:pt-4 px-2 sm:px-4 w-full md:max-w-3xl mx-auto relative scrollbar-hide'>
       <div className="chat-container">
         <div className="chat-messages">
           {/* Other user message (left side) */}
@@ -36,10 +36,118 @@ const page = () => {
               Another message from the other user.
             </p>
           </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
+          <div className="message-user">
+            <p className="message-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+            </p>
+            <div className="message-avatar-secondary">
+              U
+            </div>
+          </div>
+
+          {/* Another other user message */}
+          <div className="message-other">
+            <div className="message-avatar-primary">
+              N
+            </div>
+            <p className="message-text">
+              Another message from the other user.
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="sticky bottom-0 w-full max-w-3xl z-10 px-2 sm:px-0">
+      <div className="fixed bottom-0 w-full max-w-3xl z-10 px-2 sm:px-0">
         <AiInput />
       </div>
     </section>
